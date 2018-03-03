74447a2e48990b38d721ec63222253f085a04a3a
Glitch (watery-butter) <none>
2018-03-03 06:35:21 +0000

    💦👏 Checkpoint README.md:138322/199 package.json:138322/221 server.js:138322/255

--- README.md
+++ README.md
@@ -1,27 +1,7 @@
-Welcome to the Glitch BETA
-=========================
+# KEXP now playing
 
-Click `Show` in the header to see your app live. Updates to your code will instantly deploy and update live.
+Not associated with https://kexp.org. I just use their API.
 
-**Glitch** is the friendly community where you'll build the app of your dreams. Glitch lets you instantly create, remix, edit, and host an app, bot or site, and you can invite collaborators or helpers to simultaneously edit code with you.
+Support them: https://kexp.org/donate.
 
-Find out more [about Glitch](https://glitch.com/about).
-
-
-Your Project
-------------
-
-On the front-end,
-- edit `public/client.js`, `public/style.css` and `views/index.html`
-- drag in `assets`, like images or music, to add them to your project
-
-On the back-end,
-- your app starts at `server.js`
-- add frameworks and packages in `package.json`
-- safely store app secrets in `.env` (nobody can see this but you and people you invite)
-
-
-Made by [Fog Creek](https://fogcreek.com/)
--------------------
-
-\ ゜o゜)ノ
+March 2018, Emma Humphries
--- server.js
+++ server.js
@@ -1,37 +1,23 @@
-// server.js
-// where your node app starts
+const fetch = require('node-fetch');
+const express = require('express');
+const app = express();
+var   data = JSON.stringify({
+  "message": "no data yet"
+});
 
-// init project
-var express = require('express');
-var app = express();
-
-// we've started you off with Express, 
-// but feel free to use whatever libs or frameworks you'd like through `package.json`.
-
-// http://expressjs.com/en/starter/static-files.html
 app.use(express.static('public'));
 
-// http://expressjs.com/en/starter/basic-routing.html
 app.get("/", function (request, response) {
   response.sendFile(__dirname + '/views/index.html');
 });
 
-app.get("/dreams", function (request, response) {
-  response.send(dreams);
+app.get("/data", function (request, response) {
+  response.send(data);
 });
 
-// could also use the POST body instead of query string: http://expressjs.com/en/api.html#req.body
-app.post("/dreams", function (request, response) {
-  dreams.push(request.query.dream);
-  response.sendStatus(200);
-});
-
-// Simple in-memory store for now
-var dreams = [
-  "Find and count some sheep",
-  "Climb a really tall mountain",
-  "Wash the dishes"
-];
+function fetchData() {
+  fetch( 
+}
 
 // listen for requests :)
 var listener = app.listen(process.env.PORT, function () {
