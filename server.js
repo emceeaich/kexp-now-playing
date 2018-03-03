d41e9e0ec27431797daed30be29d5ea18faae07f
Glitch (kexp-now-playing) <none>
2018-03-03 06:45:26 +0000

    🎁🎇 Checkpoint server.js:138322/544

--- server.js
+++ server.js
@@ -2,7 +2,8 @@ const fetch = require('node-fetch');
 const express = require('express');
 const app = express();
 var   data = JSON.stringify({
-  "message": "no data yet"
+  "message": "No data yet",
+  "status" : "not-ready"
 });
 
 app.use(express.static('public'));
@@ -15,9 +16,28 @@ app.get("/data", function (request, response) {
   response.send(data);
 });
 
-function fetchData() {
-  fetch( 
+function update() {
+  fetch("https://legacy-api.kexp.org/play/?format=json&limit=1&ordering=-airdate")
+    .then(response => {
+      if (response.ok)
+      response.json()
+      .then(data => {
+        data.message = "Okay",
+        data.status = 'ready'
+      })
+      .error(err => {
+        data = JSON.stringify({
+            "message": `Error getting data: ${err}`,
+            "status": 'not-ready'
+        });
+      });
+  })
 }
+          
+var timeout = setInterval(update, 60*1*1000);
+
+// get first set of data
+update();
 
 // listen for requests :)
 var listener = app.listen(process.env.PORT, function () {
