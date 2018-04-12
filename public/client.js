f10b862b2d86ee90923623beeca713ae2dc2569e
Glitch (kexp-now-playing) <none>
2018-04-12 04:57:59 +0000

    🎰💣 Checkpoint ./public/client.js:138322/538

--- public/client.js
+++ public/client.js
@@ -1,18 +1,15 @@
 var main, footer;
     
 $(function() {
-    main = document.querySelector('main');
-    footer = document.querySelector('footer ul');
+    main = $('div.main');
+    footer = $('div.footer ul');
     renderData();
     window.setInterval(renderData, 60*1000);
   }
 );
   
 function renderData() {
-  $.ajax('/data'
-  fetch('/data')
-
-        
+  $.getJSON('/data', function(data) {
         if (data.status === 'not-ready') {
           main.innerHTML = "<p>No data.</p>";
           return;
@@ -23,30 +20,30 @@ function renderData() {
         var li;
         
         // clean up 
-        main.innerHTML = "";
+        main.innerHTML = '';
         if (data.release && data.release.largeimageuri) {
-          html += `<img class="album-art" src="${data.release.largeimageuri}" alt="album art">`;
+          html += '<img class="album-art" src=' + data.release.largeimageuri + '" alt="album art">';
         } else {
           html += '<img class="album-art" src="https://kexp.org/static/assets/img/default.png" alt="no album art">'; 
         }
         
-        html += `<div class="track">
-                   <h2>${data.artist.name}<h2>
-                   <h3>${data.track.name}</h3>`;
+        html += '<div class="track">'
+                  + '<h2>' + data.artist.name + '<h2>'
+                  + '<h3>' + data.track.name + '</h3>';
         
         if (data.release && data.release.name) {
-          html += `<h4>${data.release.name}</h4>`; 
+          html += '<h4>' + data.release.name + '</h4>'; 
         }
         
         html +=  "</div>";
         
         if (data.comments && data.comments.length > 0) {
-          html += `<div class="comment">
-                     <p>${data.comments[0].text}</p>
-                   </div>`;
+          html += '<div class="comment">'
+                  + '<p>' + data.comments[0].text + '</p>'
+                  + '</div>';
         }
         
-        main.insertAdjacentHTML('beforeend', html);
+        main.innerHTML = html;
         
         // Timestamp
         timestamp = new Date(data.airdate).toLocaleString();
@@ -54,10 +51,6 @@ function renderData() {
         if (li) {
           li.remove();
         }
-        footer.insertAdjacentHTML('beforeend', `<li class="timestamp">Last update ${timestamp}</li>`);
-        
-      });
-    }
-  })
-  .catch();
+        footer.innerHTML = '<li class="timestamp">Last update ' + timestamp + '</li>';   
+  });
 }
