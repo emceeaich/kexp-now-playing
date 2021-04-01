2df6fb1f886f60c08ac717f7b5cea6e4a33fc00f
Glitch (kexp-now-playing) <none>
2021-04-01 02:27:41 +0000

    🐦🏚 Checkpoint ./server.js:138322/3323

--- server.js
+++ server.js
@@ -1,37 +1,43 @@
-const fetch = require('node-fetch');
-const express = require('express');
+const fetch = require("node-fetch");
+const express = require("express");
 const app = express();
-var   data = {
-  "message": "No data yet",
-  "status" : "not-ready"
+var data = {
+  message: "No data yet",
+  status: "not-ready"
 };
-var   id = null;
+var id = null;
 
-app.use(express.static('public'));
+app.use(express.static("public"));
 
-app.get("/", function (request, response) {
-  response.sendFile(__dirname + '/views/index.html');
+app.get("/", function(request, response) {
+  response.sendFile(__dirname + "/views/index.html");
 });
 
-app.get("/data", function (request, response) {
+app.get("/data", function(request, response) {
   response.send(data);
 });
 
 function update() {
-  fetch("https://legacy-api.kexp.org/play/?format=json&ordering=-airdate", {
-    headers: { "User-Agent": "kexp-now-playing app, contact Emma Humphries <ech@emmah.net>" }
-  }) 
-    .then(response => {
-      if (response.ok) {
-        response.json()
+  console.debug('in update')
+  fetch("https://legacy-api.kexp.org/play/?format=json&limit=5ordering=-airdate", {
+/*
+#    headers: {
+#      "User-Agent":
+#        "kexp-now-playing app, contact Emma Humphries <ech@emmah.net>"
+*/
+    }
+  }).then(response => {
+    if (response.ok) {
+      response
+        .json()
         .then(newData => {
           var result = newData.results.find(result => {
-            return result.artist;  // return the most recent artist played, skipping air breaks and promos
+            return result.artist; // return the most recent artist played, skipping air breaks and promos
           });
-          var newId  = result.playid;
+          var newId = result.playid;
           if (result.artist && newId !== id) {
             result.message = "Okay";
-            result.status = 'ready';
+            result.status = "ready";
             data = result;
             id = newId;
           }
@@ -39,19 +45,21 @@ function update() {
         })
         .catch(err => {
           data = {
-              "message": `Error getting data: ${err}`,
-              "status": 'not-ready'
+            message: `Error getting data: ${err}`,
+            status: "not-ready"
           };
         });
-      }
-  })
-}           
-var timeout = setInterval(update, 60*1000);
+    } else {
+      console.error(response.status, response.statusText)
+    }
+  });
+}
+var timeout = setInterval(update, 60 * 1000);
 
 // get first set of data
 update();
 
 // listen for requests :)
-var listener = app.listen(process.env.PORT, function () {
-  console.log('Your app is listening on port ' + listener.address().port);
+var listener = app.listen(process.env.PORT, function() {
+  console.log("Your app is listening on port " + listener.address().port);
 });
